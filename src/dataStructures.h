d1afa7140c7c440c5da5479f6fa04ee483f5af2e
gas <gasparian>
2019-12-29 17:48:26 +0300

    Circular buffer data-structure added

--- src/dataStructures.h
+++ src/dataStructures.h
@@ -2,6 +2,7 @@
 #define dataStructures_h
 
 #include <vector>
+#include <algorithm>
 #include <opencv2/core.hpp>
 
 
@@ -14,5 +15,33 @@ struct DataFrame { // represents the available sensor information at the same ti
     std::vector<cv::DMatch> kptMatches; // keypoint matches between previous and current frame
 };
 
+template<typename T> 
+class CircBuf {
+public:
+  CircBuf(int sizeIn) { buf_size = sizeIn; };
+  int size() const { return count; }
+  bool empty() const { return count == 0; }
+  bool full() const { return count == buf_size; }
+  void remove() {
+      if ( !empty() )
+        data.erase(data.begin());
+        count--;
+  }
+  void add(const T& inp) {
+      if ( full() )
+        remove();
+      data.push_back(inp);
+      count++;
+  }
+  typename std::vector<T>::iterator getItem(int n) {
+    n = std::min({n, buf_size});
+    return (data.end() - n);
+  }
+
+private:
+  int buf_size = 0;
+  int count = 0;
+  std::vector<T> data;
+};
 
 #endif /* dataStructures_h */
